d45124efc03f33f9934387f4ba71c36a41979256
suhel khan8080@gmail.com <suhailali8080@gmail.com>
2026-09-15 22:45:09 +0530

    learn concept of GET PUT DELETE etc.

---
+++ controllers/deleteTodo.js
@@ -0,0 +1,32 @@
+const Todo = require("../models/Todo");
+//default route handler
+
+exports.deleteTodo =async(req, res) =>{
+    try{
+        //extract lital and description from reauest body
+        const {title, description} = req.body;
+          //create a new Todo Obj and insert in DB
+          const  response = await Todo.create({title,description});
+          // send a json response with a succes flag
+            res.status (200).json({
+                success: true,
+                data:response,
+                message: 'Enter Created Successfully'
+            }
+        );
+
+    }
+
+    catch(err){
+        console.error(err);
+        console.log(err);
+        res.status(500)
+        .json({
+            success: false,
+            data: "Internal Server Error",
+            message: err.message,
+
+        })
+
+    }
+}
---
+++ controllers/getTodo.js
@@ -0,0 +1,73 @@
+const Todo = require("../models/Todo");
+
+
+//default route handler
+
+exports.getTodo =async(req, res) =>{
+    try{
+
+        //featch all todo items in database
+       const todos = await Todo.find({});
+
+       //response
+       res.status(200)
+       .json({
+        success: true,
+        data:todos,
+        message:"Enter Todo Data is Fetched",
+
+       });
+    }
+
+    catch(err){
+         console.log(err);
+        res.status(500)
+        .json({
+            success: false,
+            error:err.message,
+            message:"Server Error",
+             })    
+    }
+}
+
+
+exports.getTodoById = async(req,res) =>{
+    try {
+        //extrect Todo item basice on id
+
+        const id = req.params.id;
+        const todo = await Todo.findById({_id:id})
+
+        //data forgiven id not found
+
+       if(!todo){
+        return res.status(404).json({
+            success: false,
+            message:"No Data Found With Given Id",
+        })
+        //data for given id found
+        res.status(200).json({
+            success:true,
+            data:todo,
+            message:`Todo ${id} data successfully fetched`
+        })        
+
+       }
+
+
+    }
+
+    catch(err){
+
+          console.log(err);
+        res.status(500)
+        .json({
+            success: false,
+            error:err.message,
+            message:"Server Error",
+             })    
+
+    }
+
+
+}
