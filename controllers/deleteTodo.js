53c8b50cbefe6cdca430b3b6c82bfe76769d776c
suhel khan8080@gmail.com <suhailali8080@gmail.com>
2026-09-16 17:10:18 +0530

    DELETE command using

--- controllers/deleteTodo.js
+++ controllers/deleteTodo.js
@@ -3,30 +3,24 @@ const Todo = require("../models/Todo");
 
 exports.deleteTodo =async(req, res) =>{
     try{
-        //extract lital and description from reauest body
-        const {title, description} = req.body;
-          //create a new Todo Obj and insert in DB
-          const  response = await Todo.create({title,description});
-          // send a json response with a succes flag
-            res.status (200).json({
-                success: true,
-                data:response,
-                message: 'Enter Created Successfully'
-            }
-        );
-
+        const {id} = req.params;
+        await Todo.findByIdAndDelete(id);
+        res.json({
+            success:true,
+            message:"Todo  DELETED",
+        })
     }
 
     catch(err){
-        console.error(err);
-        console.log(err);
+           console.error(err);
         res.status(500)
         .json({
             success: false,
-            data: "Internal Server Error",
-            message: err.message,
+            message: "Server Error",
+            error: err.message,
 
-        })
+        });
+     
 
     }
 }
